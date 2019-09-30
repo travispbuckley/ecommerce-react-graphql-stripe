8e217722f8d618650aee6eb1e317d924d14e3a4d
Travis Buckley <travispbuckley@lewisu.edu>
2019-09-29 19:14:47 -0500

    update App component with container/box for main heading

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { Container, Box, Heading } from 'gestalt';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <Container>
+        {/* Brands */}
+        <Box
+          display="flex"
+          justifyContent="center"
+          marginBottom={2}
+        >
+          <Heading color="midnight" size="md">
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
     );
   }
 }
